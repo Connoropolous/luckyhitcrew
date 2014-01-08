1d9264c3f55bee7a444e22ec8a637f462c14d646
OpenShift guest <connorturland@gmail.com>
2014-01-08 07:27:18 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luckyhitcrew::Application.config.secret_token = 'dd0878a63d1b0412d5ec3993051a2f43cd32ba621c80900fcfb414c48a99f8ba6ce153e8a723427b641948e3a07b3907ed3883beefcdab5049bddd43f5160281'
