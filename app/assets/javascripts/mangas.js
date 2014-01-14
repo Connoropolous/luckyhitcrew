24a06c2d1521d24df7dc9e43b962d0c1786e4ae0
Connor Turland <connor@madhattertech.ca>
2014-01-14 15:19:57 -0500

    removed problematic jquery related to rating

--- app/assets/javascripts/mangas.js
+++ app/assets/javascripts/mangas.js
@@ -1,26 +1,4 @@
 $(document).ready(function(){
 
-	$('#delete-confirm').on('show', function() {
-	  var $submit = $(this).find('.btn-danger'),
-	      href = $submit.attr('href');
-	  
-	  $submit.attr('href', href.replace('pony', $(this).data('id')));
-	});
-
-	$('.delete-confirm').click(function(e) {
-	  e.preventDefault();
-	  $('#delete-confirm').data('id', $(this).data('id')).modal('show');
-	});
-
-	$('.auto-submit-star').rating({
-		callback: function(value, link){
-			$.ajax({
-				type: 'post',
-				url: 'mangas/rate',
-				dataType: 'json',
-				data: { manga_rating: value }
-			});
-		}
-	});
 
 });
